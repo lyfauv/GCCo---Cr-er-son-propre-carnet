ee39ca1feca5491c589a5026055a09024efb089d
lylia fauvel <lylfauvel@ensc.fr>
2018-05-10 11:54:27 +0200

    Modifs OIDC

--- OIDC.php
+++ OIDC.php
@@ -3,7 +3,16 @@
 	$title = 'OIDC';
 ?>
 
-	
+	<div class = "container">
+		<h2>Diagramme OIDC</h2>
+		<p class="texte">Ci-après se trouve la mind map présentant les notions nécessaires à la création d'un carnet.
+		<br/>Les <b>entrées</b> et les <b>sorties</b> caractérisent ce qui est nécéssaire à la réalisation de l'action et ce qu'on est censé obtenir après réalisation complète des tâches à effectuer.
+		<br/>Le <b>système opérant</b> représente le qui ou le quoi qui va effectuer l'action.
+		<br/>Dans notre cas ce sera donc le qui ou le quoi qui va fabriquer le carnet.
+		<br/>Le <b>système d'information</b> représente les moyens par lesquels on peut avoir des informations concernant les procédures à effectuer pour réaliser l'action décrite.
+		<br/>Le <b>système de décision</b> énumère les agents à l'origine de diverses décisions par rapport à l'action effectuée (dans notre cas par exemple, désigne l'agent qui va décider de la couleur de la couverture).
+		<br/>Enfin, le <b>patrimoine de connaissance</b> regroupe tous les types de savoir et savoirs nécessaires à la réalisation de l'action.
+	</div>
 	
 <?php 
 	include('includes/pied.php');
