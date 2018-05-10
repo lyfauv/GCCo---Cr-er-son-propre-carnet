ce6c9f36fa91df8efd2c63a28bfbba7682409ff1
Yen <hoang-yen.pham@hotmail.fr>
2018-05-11 01:18:44 +0200

    Ajout liens téléchargement

--- phenomenes.php
+++ phenomenes.php
@@ -22,24 +22,35 @@
 				
 				<p>Dans le cas de la création de son propre carnet, nous faisons face à trois phénomènes importants : le pli, la découpe et le collage.</p>
 				
+				<p> L'ensemble des trois diagrammes peut être téléchargé <a href="telechargements/phenomenes.zip" download="diagramme_phenomenes">ici</a>.</p>
+				
 				<h2 id="pli">Phénomène de pli</h2>
 				<p>La première étape de la création de carnet consiste à plier les feuilles A4 en 4. Cela permet de marquer des lignes visibles qui se placent au milieu d'une feuille sans effectuer de mesure explicite.</p>
-				<a href="diagramme/large/pli.png" title="Diagramme du phénomène de pli" data-spzoom>
-					<img src="diagramme/petit/pli.png" alt="thumb">
-				</a>
+				<div class="center">
+					<a href="diagramme/large/pli.png" title="Diagramme du phénomène de pli" data-spzoom>
+						<img src="diagramme/petit/pli.png" alt="thumb">
+					</a><br/><br/>
+					<a href="telechargements/phenomene_pli.zip" download="phenomene_pli">>> Télécharger le diagramme du phénomène de pli</a><br/><br/>
+				</div>
 
 				<h2 id="decoupe">Phénomène de découpe</h2>
 				<p>Après avoir effectué le pli, nous passons alors à la découpe. Dans notre activité, l'objectif est de passer d'une feuille, à 4 feuilles de dimensions plus petites.</p>
-				<a href="diagramme/large/decoupe.png" title="Diagramme du phénomène de pli" data-spzoom>
-					<img src="diagramme/petit/decoupe.png" alt="thumb">
-				</a>
+				<div class="center">
+					<a href="diagramme/large/decoupe.png" title="Diagramme du phénomène de pli" data-spzoom>
+						<img src="diagramme/petit/decoupe.png" alt="thumb">
+					</a><br/><br/>
+					<a href="telechargements/phenomene_decoupe.zip" download="phenomene_decoupe">>> Télécharger le diagramme du phénomène de découpe</a><br/><br/>
+				</div>
 				
 				<h2 id="collage">Phénomène de collage</h2>
 				<p>Le collage est alors un point crucial de la réalisation du carnet (selon la méthode que nous avons choisie).
 				Nous nous en servons à la fois pour fixer les feuilles entre elles sur un côté commun, mais aussi pour fixer l'ensemble de feuille à sa couverture.</p>
-				<a href="diagramme/large/colle.png" title="Diagramme du phénomène de pli" data-spzoom>
-					<img src="diagramme/petit/colle.png" alt="thumb">
-				</a>
+				<div class="center">
+					<a href="diagramme/large/colle.png" title="Diagramme du phénomène de pli" data-spzoom>
+						<img src="diagramme/petit/colle.png" alt="thumb">
+					</a><br/><br/>
+					<a href="telechargements/phenomene_colle.zip" download="phenomene_colle">>> Télécharger le diagramme du phénomène de collage</a>
+				</div>
 			</div>
 		</div>
 	</div>
--- taches.php
+++ taches.php
@@ -8,9 +8,13 @@
     <p class="texte">Le diagramme des tâches permet de préciser toutes les étapes intermédiaires nécessaires à la réalisation des activités. 
     <br/>On représente ici par un rond noir les tâches exécutées de façon sérielle, et les droites parallèles représentent les tâches effectuées en parallèle.
 	<br/><br/><br/><br/><br/>
-	<a href="diagramme/large/taches.png" title="Diagramme des taches" data-spzoom>
-		<img src="diagramme/petit/taches.png" alt="thumb">
-	</a>
+	<div class="center">
+		<a href="diagramme/large/taches.png" title="Diagramme des taches" data-spzoom>
+			<img src="diagramme/petit/taches.png" alt="thumb">
+		</a><br/><br/>
+		<a href="telechargements/taches.zip" download="phenomene_decoupe">>> Télécharger le diagramme des tâches</a>
+	</div>
+	
 
 	<br/><br/><br/><br/><br/>
 	</div>
