b24160b7e73ba40487c66a2177c03deca9ac52fd
lylia fauvel <lylfauvel@ensc.fr>
2018-05-10 14:13:25 +0200

    Dernières modifs

--- activite.php
+++ activite.php
@@ -1,3 +1,9 @@
 <?php include "includes/entete.php";?>
 
+<div class = "container">
+    <h2>Diagramme des activités</h2>
+    <p class="texte">Le diagramme ci-après détaille les grandes étapes nécessaire à la fabrication d'un carnet.
+    <br/>Il permet notamment de préciser pour chaque action à effectuer, ce qui va être transformé ainsi que le résultat censé être obtenu après transformation.
+</div>
+
 <?php include "includes/pied.php";?>
--- phenomenes.php
+++ phenomenes.php
@@ -18,13 +18,13 @@
 				
 				<p>Dans le cas de la création de son propre carnet, nous faisons face à trois phénomènes importants : le pli, la découpe et le collage.</p>
 				
-				<h2>Phénomène de pli</h2>
+				<h2 id="pli">Phénomène de pli</h2>
 				<p>La première étape de la création de carnet consiste à plier les feuilles A4 en 4. Cela permet de marquer des lignes visibles qui se placent au milieu d'une feuille sans effectuer de mesure explicite.</p>
 				
-				<h2>Phénomène de découpe</h2>
+				<h2 id="decoupe">Phénomène de découpe</h2>
 				<p>Après avoir effectué le pli, nous passons alors à la découpe. Dans notre activité, l'objectif est de passer d'une feuille, à 4 feuilles de dimensions plus petites.</p>
 				
-				<h2>Phénomène de collage</h2>
+				<h2 id="collage">Phénomène de collage</h2>
 				<p>Le collage est alors un point crucial de la réalisation du carnet (selon la méthode que nous avons choisie).
 				Nous nous en servons à la fois pour fixer les feuilles entre elles sur un côté commun, mais aussi pour fixer l'ensemble de feuille à sa couverture.</p>
 			</div>
